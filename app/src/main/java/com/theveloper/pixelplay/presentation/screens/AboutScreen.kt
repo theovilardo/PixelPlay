a92f76c6714805b1c8f795bdbeedbe29bae7ab16
theo <theovilardo@gmail.com>
2025-09-08 23:47:01 -0300

    feat: Update AboutScreen app icon
    This commit updates the app icon displayed on the `AboutScreen`.
    
    The key changes are:
    - The app icon resource is changed from `R.drawable.monochrome` to `R.drawable.new_monochrome`.
    - The size of the icon is increased from `128.dp` to `228.dp`.
    - A `colorFilter` is applied to tint the icon with the primary color of the current MaterialTheme.

--- app/src/main/java/com/theveloper/pixelplay/presentation/screens/AboutScreen.kt
+++ app/src/main/java/com/theveloper/pixelplay/presentation/screens/AboutScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -69,10 +70,11 @@ fun AboutScreen(navController: NavController) {
                     modifier = Modifier.padding(bottom = 24.dp)
                 ) {
                     Image(
-                        painter = painterResource(id = R.drawable.monochrome),
+                        painter = painterResource(id = R.drawable.new_monochrome),
                         contentDescription = "App Icon",
+                        colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.primary),
                         modifier = Modifier
-                            .size(128.dp)
+                            .size(228.dp)
                             .padding(bottom = 16.dp)
                     )
                     Text(
